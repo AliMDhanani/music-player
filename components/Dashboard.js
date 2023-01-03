a80662bd800cfc38a35bd5deee3c2c677f5f9352
AliMDhanani <alidhanani2000@gmail.com>
2023-01-03 18:38:43 +0000

    poster component

--- components/Dashboard.js
+++ components/Dashboard.js
@@ -1,13 +1,26 @@
 import React from "react";
+import { useRecoilState } from "recoil";
+import SpotifyWebApi from "spotify-web-api-node";
+import { playingTrackState } from "../atoms/playerAtom";
 import Body from "./Body";
 import Right from "./Right";
 import Sidebar from "./Sidebar";
 
+const spotifyApi = new SpotifyWebApi({
+  clientId: process.env.SPOTIFY_CLIENT_ID,
+});
+
 const Dashboard = () => {
+  const [playingTrack, setPlayingTrack] = useRecoilState(playingTrackState);
+
+  const chooseTrack = (track) => {
+    setPlayingTrack(track);
+  };
+
   return (
     <main>
       <Sidebar />
-      <Body />
+      <Body spotifyApi={spotifyApi} chooseTrack={chooseTrack}/>
       <Right />
     </main>
   );
