92ffb958697f155fc6e98f5cd48d0c92b7d6f0ec
AliMDhanani <alidhanani2000@gmail.com>
2023-01-03 21:46:35 +0000

    env updated

--- components/Loader.js
+++ components/Loader.js
@@ -5,7 +5,7 @@ function Loader() {
   return (
     <div className="h-screen bg-black">
       <div className="pt-40 flex flex-col items-center space-y-4">
-        <span className="relative w-[400px] h-[250px] lg:w-[550px] lg:h-[240px]">
+        <span className="relative w-[400px] h-[251px] lg:w-[550px] lg:h-[240px]">
           <Image
             src="https://rb.gy/y9mwtb"
             layout="fill"
